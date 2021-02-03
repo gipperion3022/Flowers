bd3f83e3219521e2c186d462b370bced1aaf4176
gipperion3022 <4660216@gmail.com>
2021-02-03 22:22:52 +0900

    two lesson

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const flowersSlider = new Swiper('.flowers_slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  
+
+  // стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+});
